39df619e7026cb4c9d714d5f967406a7e3193f72
Miguel da Silva <miguel.dasilva@emea.nttdata.com>
2023-08-04 18:39:24 +0200

    Adding script for launching Nexus container

--- templates/shared_services/sonatype-nexus-vm/scripts/launch_nexus_container.sh
+++ templates/shared_services/sonatype-nexus-vm/scripts/launch_nexus_container.sh
@@ -3,13 +3,13 @@ set -o pipefail
 set -o nounset
 # set -o xtrace
 
-counter=0
+counter=1
 exec_number=5
 
-echo 'Pulling Sonatype Nexus image...'
-while  ! docker pull sonatype/nexus3; do
+echo "Pulling Sonatype Nexus image... Trying $counter/${exec_number}"
+while ! docker pull sonatype/nexus3; do
   # We are going to try up to exec_number times.
-  if [ $counter -gt $exec_number ]; then
+  if [ $counter -eq $exec_number ]; then
     echo 'ERROR - It was not possible to pull Sonatype Nexus image.'
     exit 1
   fi
@@ -18,9 +18,11 @@ while  ! docker pull sonatype/nexus3; do
   systemctl restart docker.service
   sleep 5
   ((counter++))
+  echo "Pulling Sonatype Nexus image... Trying $counter/${exec_number}"
 done
 
 # Run Sonatype Nexus container.
+echo 'Launching Sonatype Nexus container...'
 docker run -d -p 80:8081 -p 443:8443 -p 8083:8083 -v /etc/nexus-data:/nexus-data \
   --restart always \
   --name nexus \
