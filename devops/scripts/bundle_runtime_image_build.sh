8232639eaa85555e6052d7c82b20b0eb4cf602c2
Miguel da Silva <miguel.dasilva@emeal.nttdata.com>
2026-02-12 21:39:58 +0100

    Testing az version and docker version

--- devops/scripts/bundle_runtime_image_build.sh
+++ devops/scripts/bundle_runtime_image_build.sh
@@ -22,6 +22,8 @@ version_line=$(cat "${version_file}")
 version_array=( ${version_line//=/ } ) # split by =
 version="${version_array[1]//\"}" # second element is what we want, remove " chars
 
+echo $(az --version) 
+echo $(docker version)
 az acr login -n "${ACR_NAME}"
 
 docker_cache=("--cache-from" "${FULL_IMAGE_NAME_PREFIX}/${image_name}:${version}")
