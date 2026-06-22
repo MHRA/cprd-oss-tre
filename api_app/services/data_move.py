023159251d6aabb5c1ebdee27a9eb7668312f4db
Miguel da Silva <miguel.dasilva@emeal.nttdata.com>
2026-06-22 20:48:04 +0200

    Adding comments

--- api_app/services/data_move.py
+++ api_app/services/data_move.py
@@ -109,8 +109,8 @@ async def get_files(workspace_id: str, protocol_id: str) -> List["DataMoveFile"]
 
             relative_path = blob.name.replace(f"{constants.EXPLORE_WORKSPACE_SSBS_ORIGIN_FOLDER}/", "")
 
-            # only files in root of folder
-            # if "/" not in relative_path and not relative_path.endswith("/"):
+            # Do not add empty files to the list of files to be moved.
+            # This way we don't copy empty folders.
             if blob.size > 0 and not relative_path.endswith("/"):
                 data_move_file = DataMoveFile(
                     file_name=relative_path,
